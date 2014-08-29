c9c4a99c2d5217518f206b049e827f8d2775a454
Kare Morstol <kare@nottoobadsoftware.com>
2014-08-29 02:31:44 +0200

    Added Eclipse Public License header.

--- SwiftShell/FileHandle.swift
+++ SwiftShell/FileHandle.swift
@@ -1,10 +1,13 @@
-//
-// FileHandle.swift
-// SwiftShell
-//
-// Created by Kåre Morstøl on 17/08/14.
-// Copyright (c) 2014 NotTooBad Software. All rights reserved.
-//
+/*
+* Copyright (c) 2014 Kåre Morstøl (NotTooBad Software).
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Kåre Morstøl, https://github.com/kareman - initial API and implementation.
+*/
 
 import Foundation
 
