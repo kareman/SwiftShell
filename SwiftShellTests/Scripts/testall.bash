b111afa8d8bd26bdafb33b1f5ef9490a5a3d5db2
Kare Morstol <kare@nottoobadsoftware.com>
2014-09-24 21:15:25 +0200

    Add bash script to test all Swift scripts.

---
+++ SwiftShellTests/Scripts/testall.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# http://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
+# Exit on any error.
+set -e
+# Exit on any error in a pipeline.
+set -o pipefail
+
+./callswiftscriptfromswift.swift
+#./exitswhenopeningnon-existentfile.swift
+./listallexecutablesinpath.swift
+./print_arguments.swift 1 2
+./print_environment.swift
+ls | ./print_linenumbers.swift
+./readfilelinebyline.swift
+./stream_out.swift
