6be3d656e329b76fdd28251013cdbdd62245424e
Kare Morstol <kare@nottoobadsoftware.com>
2014-11-21 20:19:00 +0100

    Clean up playground.

--- Div/Test area.playground/section-1.swift
+++ Div/Test area.playground/section-1.swift
@@ -3,11 +3,3 @@
 
 import SwiftShell
 
-let (input,output) = streams()
-
-input.writeln("10.17.14,15:58:00,Magesmerte,Middels,Ut dagen. Spiste noe lell.")
-input.writeln("10.14.14,16:49:00,Magesmerte,Middels,\"Spiste skive med brunost , så skive med peanøttsmør og kvart eple. Etter jobb, før trening.\" ")
-input.writeln("10.14.14,11:49:28,Drit,Middels,")
-input.closeStream()
-
-output.lines() |> map { $0.replace(",","\t",limit: 4) } |> writeTo(standardoutput)
