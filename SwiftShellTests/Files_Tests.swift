fcd7eba2a4a95048b6f83419810e70c33fbf334f
Kare Morstol <kare@nottoobadsoftware.com>
2014-12-01 22:43:03 +0100

    Fix horrendous use of spaces for indentation.

--- SwiftShellTests/Files_Tests.swift
+++ SwiftShellTests/Files_Tests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 class Files_Tests: XCTestCase {
 
-  func testTempDirectoryIsTheSameAfterRepeatedCalls () {
+	func testTempDirectoryIsTheSameAfterRepeatedCalls () {
 		XCTAssertEqual(tempdirectory, tempdirectory)
 	}
 }
