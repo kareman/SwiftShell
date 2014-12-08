a4aa7ca9f8b4d67242fb2648af5d158e258e0d0b
Kare Morstol <kare@nottoobadsoftware.com>
2014-12-08 18:36:22 +0100

    Clean up speed iteration speed tests. Still never finishes when built for Release.

--- SwiftShell_Speed_Tests/Stream_Iteration_SpeedTests.swift
+++ SwiftShell_Speed_Tests/Stream_Iteration_SpeedTests.swift
@@ -15,14 +15,14 @@ class Stream_Iteration_SpeedTests: XCTestCase {
 
 	func testSpeedNSStringSplit () {
 
-		// won't work outside of a method (6.1 beta 2)
+		// won't work outside of a method (XCode 6.1.1 6A2006)
 		let longtextpath = pathForTestResource("long text", type: "txt")
 
 		self.measureBlock() {
 			let f = open(longtextpath)
 			let text = f.read()
 			let array = (text.split("\n"))
-			
+
 			var i = 0
 			var result = ""
 			for line in array {
@@ -31,17 +31,17 @@ class Stream_Iteration_SpeedTests: XCTestCase {
 			}
 			XCTAssertEqual(i, self.numberoflines)
 			XCTAssert(result != "")
-			f.closeFile()
+			(f as NSFileHandle).closeFile()
 		}
 	}
-	
+
 	// takes a very long time
 	func testSpeedSwiftSplit () {
 		let longtextpath = pathForTestResource("long text", type: "txt")
 		self.measureBlock(){
 			let f = open(longtextpath)
 			let text = f.read()
-			
+
 			var i = 0
 			var result = ""
 			for line in split(text, { $0 == "\n"}, allowEmptySlices: true) {
@@ -50,15 +50,15 @@ class Stream_Iteration_SpeedTests: XCTestCase {
 			}
 			XCTAssertEqual(i, self.numberoflines)
 			XCTAssert(result != "")
-			f.closeFile()
+			(f as NSFileHandle).closeFile()
 		}
 	}
-	
+
 	func testSpeedSwiftShellSplit () {
 		let longtextpath = pathForTestResource("long text", type: "txt")
 		self.measureBlock() {
 			let f = open(longtextpath)
-			
+
 			var i = 0
 			var result = ""
 			for line in f.lines() {
@@ -67,40 +67,39 @@ class Stream_Iteration_SpeedTests: XCTestCase {
 			}
 			XCTAssertEqual(i, self.numberoflines)
 			XCTAssert(result != "")
-			f.closeFile()
+			(f as NSFileHandle).closeFile()
 		}
-	}	
-	
+	}
+
 	func allSpeedsSplitFileAsString() -> Array<UInt64> {
 		let longtextpath = pathForTestResource("long text", type: "txt")
 
 		// set initial size of array.
 		var times = Array<UInt64>(count: numberoflines, repeatedValue: 0)
-		
+
 		let f = open(longtextpath)
 		let start = mach_absolute_time()
 
 		let text = f.read()
 		let array = (text.split("\n"))
-		
+
 		var i = 0
 		var result = ""
 		for line in array {
-			
 			times[i++] = (mach_absolute_time() - start)
 			result += line
 		}
-		
-	XCTAssertEqual(i, numberoflines)
+
+		XCTAssertEqual(i, numberoflines)
 		XCTAssert(result != "")
-		f.closeFile()
+		(f as NSFileHandle).closeFile()
 		return times
 	}
-	
+
 	func allSpeedIterateOverFile() -> Array<UInt64>{
 		let longtextpath = pathForTestResource("long text", type: "txt")
 		var times = Array<UInt64>(count: numberoflines, repeatedValue: 0)
-		
+
 		let f = open(longtextpath)
 		let start = mach_absolute_time()
 
@@ -112,10 +111,10 @@ class Stream_Iteration_SpeedTests: XCTestCase {
 		}
 		XCTAssertEqual(i, numberoflines)
 		XCTAssert(result != "")
-		f.closeFile()
+		(f as NSFileHandle).closeFile()
 		return times
 	}
-	
+
 	func testWhenSplitFileAsStringBecomesQuicker() {
 		let splitarray = allSpeedsSplitFileAsString()
 		let myarray = allSpeedIterateOverFile()
