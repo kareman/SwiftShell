7c443bfb1a4296c74526774328466d70043ba9a8
Kare Morstol <kare@nottoobadsoftware.com>
2015-08-05 20:26:50 +0200

    Add protocol ShellContextType and let “main” implement it.
    - make “main” a true singleton.

--- SwiftShell/Context.swift
+++ SwiftShell/Context.swift
@@ -7,11 +7,31 @@
 
 import Foundation
 
-public struct Context {
+public protocol ShellContextType {
+	var encoding: UInt {get set}
+	var env: [String: String] {get set}
+
+	var stdin: NSFileHandle {get set}
+	var stdout: NSFileHandle {get set}
+	var stderror: NSFileHandle {get set}
+
+	/**
+	The current working directory.
+
+	Must be used instead of `run("cd ...")` because all the `run` commands are executed in a
+	separate process and changing the directory there will not affect the rest of the Swift script.
+
+	This directory is also used as the base for relative URL's.
+	*/
+	var currentdirectory: String {get set}
+}
+
+
+public struct MainShellContext: ShellContextType {
 
 	// TODO: get encoding from environmental variable LC_CTYPE
 	public var encoding = NSUTF8StringEncoding
-	public lazy var env = NSProcessInfo.processInfo().environment as [String: String]
+	public var env = NSProcessInfo.processInfo().environment as [String: String]
 
 	public var stdin    = NSFileHandle.fileHandleWithStandardInput() //as ReadableStreamType
 	public var stdout	  = NSFileHandle.fileHandleWithStandardOutput() //as WriteableStreamType
@@ -36,6 +56,10 @@ public struct Context {
 
 	public lazy var arguments: [String] = Process.arguments.isEmpty ? [] : Array(dropFirst(Process.arguments))
 	public lazy var name: String = Process.arguments.first ?? ""
+
+	private init() {
+
+	}
 }
 
-public var main = Context()
+public var main = MainShellContext()
