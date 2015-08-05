ee48f4c5a27b729396d9c0bf0a5354f30e8a88ed
Kare Morstol <kare@nottoobadsoftware.com>
2015-08-05 20:57:02 +0200

    Add read and write methods to NSFilehandle.

--- SwiftShell/FileHandle.swift
+++ SwiftShell/FileHandle.swift
@@ -7,3 +7,37 @@
 
 import Foundation
 
+extension NSFileHandle {
+
+	public func readSome (encoding encoding: UInt = main.encoding) -> String? {
+		let data: NSData = self.availableData
+
+		guard data.length > 0 else { return nil }
+		guard let result = NSString(data: data, encoding: encoding) else {
+			printErrorAndExit("Fatal error - could not convert stream to text.")
+		}
+
+		return result as String
+	}
+
+	public func read (encoding encoding: UInt = main.encoding) -> String {
+		let data: NSData = self.readDataToEndOfFile()
+		
+		guard let result = NSString(data: data, encoding: encoding) else {
+			printErrorAndExit("Fatal error - could not convert stream to text.")
+		}
+
+		return result as String
+	}
+}
+
+extension NSFileHandle {
+
+	public func write (string: String, encoding: UInt = main.encoding) {
+		writeData(string.dataUsingEncoding(encoding, allowLossyConversion:false)!)
+	}
+
+	public func writeln (string: String = "", encoding: UInt = main.encoding) {
+		self.write(string + "\n", encoding: encoding)
+	}
+}
