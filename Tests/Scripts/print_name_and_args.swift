03fd4a20af15b6390743d69e65eaa6051857763d
Kare Morstol <kare@nottoobadsoftware.com>
2016-05-25 03:30:36 +0200

    Appease Swift 3s stricter rules about importing Foundation.

--- Tests/Scripts/print_name_and_args.swift
+++ Tests/Scripts/print_name_and_args.swift
@@ -1,5 +1,6 @@
 #!/usr/bin/env swiftshell
 
 import SwiftShell
+import Foundation 
 
 print(NSURL(fileURLWithPath: main.path).lastPathComponent!, main.arguments.map {"\""+($0)+"\""} .joined(separator:" "))
