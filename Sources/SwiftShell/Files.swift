e40e9ac014764355c936c394a426e1dec1f14149
Kare Morstol <kare@nottoobadsoftware.com>
2016-06-05 22:58:09 +0200

    Add FileError and improve error messages when opening files.

--- Sources/SwiftShell/Files.swift
+++ Sources/SwiftShell/Files.swift
@@ -16,6 +16,27 @@ public func + (leftpath: NSURL, rightpath: String) -> NSURL {
 }
 
 
+/** Error type for file commands. */
+public enum FileError: ErrorProtocol {
+
+	case notFound (path: String)
+
+	public static func checkFile (_ path: String) throws {
+		if !Files.fileExists(atPath: path) {
+			throw notFound(path: path)
+		}
+	}
+}
+
+extension FileError: CustomStringConvertible {
+	public var description: String {
+		switch self {
+		case .notFound(let path):
+			return "Error: '\(path)' does not exist."
+		}
+	}
+}
+
 /** Open a file for reading, throw if an error occurs. */
 public func open (_ path: String, encoding: NSStringEncoding = main.encoding) throws -> ReadableStream {
 	return try open(NSURL(fileURLWithPath: path, isDirectory: false), encoding: encoding)
@@ -23,7 +44,12 @@ public func open (_ path: String, encoding: NSStringEncoding = main.encoding) th
 
 /** Open a file for reading, throw if an error occurs. */
 public func open (_ path: NSURL, encoding: NSStringEncoding = main.encoding) throws -> ReadableStream {
-	return ReadableStream(try NSFileHandle(forReadingFrom: path), encoding: encoding)
+	do {
+		return ReadableStream(try NSFileHandle(forReadingFrom: path), encoding: encoding)
+	} catch {
+		try FileError.checkFile(path.path!)
+		throw error
+	}
 }
 
 /**
@@ -38,9 +64,14 @@ public func open (forWriting path: NSURL, overwrite: Bool = false, encoding: NSS
 		Files.createFile(atPath: path.path!, contents: nil, attributes: nil)
 	}
 
-	let filehandle = try NSFileHandle(forWritingTo: path)
-	filehandle.seekToEndOfFile()
-	return WriteableStream(filehandle, encoding: encoding)
+	do {
+		let filehandle = try NSFileHandle(forWritingTo: path)
+		filehandle.seekToEndOfFile()
+		return WriteableStream(filehandle, encoding: encoding)
+	} catch {
+		try FileError.checkFile(path.path!)
+		throw error
+	}
 }
 
 /**
@@ -50,5 +81,5 @@ If the file already exists and overwrite=false, the writing will begin at the en
 - parameter overwrite: If true, replace the file if it exists.
 */
 public func open (forWriting path: String, overwrite: Bool = false, encoding: NSStringEncoding = main.encoding) throws -> WriteableStream {
-	return try open(forWriting:  NSURL(fileURLWithPath: path, isDirectory: false), overwrite: overwrite, encoding:  encoding)
+	return try open(forWriting: NSURL(fileURLWithPath: path, isDirectory: false), overwrite: overwrite, encoding:  encoding)
 }
