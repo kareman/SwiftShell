38f3d41ca7584125ce86d5946659c62dccb9b2e8
Kare Morstol <kare@nottoobadsoftware.com>
2017-03-31 00:08:56 +0200

    Also rename in LinuxMain.swift.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -16,8 +16,8 @@ let tests: [XCTestCaseEntry] = [
 	testCase(RunAsync_Tests.allTests),
 	testCase(RunAndPrint_Tests.allTests),
 	testCase(MainContext_Tests.allTests),
-	testCase(CopiedShellContext_Tests.allTests),
-	testCase(BlankShellContext_Tests.allTests),
+	testCase(CopiedCustomContext_Tests.allTests),
+	testCase(BlankCustomContext_Tests.allTests),
 	testCase(Stream_Tests.allTests),
 	]
 
