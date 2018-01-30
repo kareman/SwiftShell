225b203c9e2efe50124318c799822cb6f765db07
Feng Qiang <498460828@qq.com>
2018-01-30 09:43:03 +0800

    some time self is zombie object (#45)
    * update CocoaPods version number

--- Sources/SwiftShell/Stream/Stream.swift
+++ Sources/SwiftShell/Stream/Stream.swift
@@ -119,8 +119,11 @@ extension ReadableStream {
 		/// - Note: if the stream is read from outside of the handler, or more than once inside
 		/// the handler, it may be called once when stream is closed and empty.
 		public func onOutput(_ handler: @escaping (ReadableStream) -> Void) {
-			filehandle.readabilityHandler = { [unowned self] _ in
-				handler(self)
+			filehandle.readabilityHandler = { [weak self] _ in
+                guard let wSelf = self else {
+                    return
+                }
+				handler(wSelf)
 			}
 		}
 
