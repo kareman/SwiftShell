51c31343c5c0c1f0f6d8198ea7678beaef6fafbc
Kare Morstol <kare@nottoobadsoftware.com>
2018-04-08 23:25:09 +0200

    [Gardening] Cleanup Process.swift a bit.

--- Sources/SwiftShell/Process.swift
+++ Sources/SwiftShell/Process.swift
@@ -19,23 +19,22 @@ extension Process {
 		}
 		launch()
 	}
-
+	
 	/// Wait until process is finished.
 	///
 	/// - throws: `CommandError.returnedErrorCode(command: String, errorcode: Int)`
 	///   if the exit code is anything but 0.
 	public func finish() throws {
+		/// The full path to the executable + all arguments, each one quoted if it contains a space.
+		func commandAsString() -> String {
+			let path = self.launchPath ?? ""
+			return (self.arguments ?? []).reduce(path) { (acc: String, arg: String) in
+				return acc + " " + ( arg.contains(" ") ? ("\"" + arg + "\"") : arg )
+			}
+		}
 		self.waitUntilExit()
 		guard self.terminationStatus == 0 else {
-			throw CommandError.returnedErrorCode(command: commandAsString()!, errorcode: Int(self.terminationStatus))
-		}
-	}
-
-	/// The full path to the executable + all arguments, each one quoted if it contains a space.
-	func commandAsString() -> String? {
-		guard let path = self.launchPath else { return nil }
-		return self.arguments?.reduce(path) { (acc: String, arg: String) in
-			return acc + " " + ( arg.contains(" ") ? ("\"" + arg + "\"") : arg )
+			throw CommandError.returnedErrorCode(command: commandAsString(), errorcode: Int(self.terminationStatus))
 		}
 	}
 }
@@ -50,7 +49,7 @@ extension Process {
 		case blockedSignals = "blocked"
 		case ignoredSignals = "ignored"
 	}
-
+	
 	/// Gets a specified process attribute using the `ps` command installed on all Linux systems
 	///
 	/// - parameter attr: Which specific attribute to return
@@ -60,64 +59,64 @@ extension Process {
 		let attribute = run(bash: "ps --no-headers -q \(self.processIdentifier) -o \(attr.rawValue)").stdout
 		return attribute.isEmpty ? nil : attribute
 	}
-
+	
 	/// Determines whether the running process is blocking the specified signal
 	fileprivate func isBlockingSignal(_ signum: Int32) -> Bool {
 		// If there is no mask, then the signal isn't blocked
 		guard let blockedMask = getProcessInfo(.blockedSignals) else { return false }
-
+		
 		// If the output isn't in proper hexadecimal (like it should be), then
 		// it could be ignored, but we can't be sure. Return true, just to be safe
 		guard let blocked = Int(blockedMask, radix: 16) else { return true }
-
+		
 		// Checks if the signals bit in the mask is 1 (1 == blocked)
 		return blocked & (1 << signum) == 1
 	}
-
+	
 	/// Determines whether the running process is ignoring the specified signal
 	fileprivate func isIgnoringSignal(_ signum: Int32) -> Bool {
 		// If there is no mask, then the signal isn't ignored
 		guard let ignoredMask = getProcessInfo(.ignoredSignals) else { return false }
-
+		
 		// If the output isn't in proper hexadecimal (like it should be), then
 		// it could be ignored, but we can't be sure. Return true, just to be safe
 		guard let ignored = Int(ignoredMask, radix: 16) else { return true }
-
+		
 		// Checks if the signals bit in the mask is 1 (1 == ignored)
 		return ignored & (1 << signum) == 1
 	}
-
+	
 	/// Sends the specified signal to the currently running process
 	@discardableResult fileprivate func signal(_ signum: Int32) -> Int32 {
 		return kill(self.processIdentifier, signum)
 	}
-
-
+	
+	
 	/// Terminates the command by sending the SIGTERM signal
 	public func terminate() {
 		// If the SIGTERM signal is being blocked or ignored by the process,
 		// then don't bother sending it
 		guard !(isBlockingSignal(SIGTERM) || isIgnoringSignal(SIGTERM)) else { return }
-
+		
 		signal(SIGTERM)
 	}
-
+	
 	/// Interrupts the command by sending the SIGINT signal
 	public func interrupt() {
 		// If the SIGINT signal is being blocked or ignored by the process,
 		// then don't bother sending it
 		guard !(isBlockingSignal(SIGINT) || isIgnoringSignal(SIGINT)) else { return }
-
+		
 		signal(SIGINT)
 	}
-
+	
 	/// Temporarily suspends a command. Call resume() to resume a suspended command
 	///
 	/// - returns: true if the command was successfully suspended
 	@discardableResult public func suspend() -> Bool {
 		return signal(SIGTSTP) == 0
 	}
-
+	
 	/// Resumes a command previously suspended with suspend().
 	///
 	/// - returns: true if the command was successfully resumed.
